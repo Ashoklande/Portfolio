f320d5416270b8626ed0afe9fbd5d04fc3c2ffd9
ashoka <ashoklande73@gmail.com>
2025-01-23 19:22:53 +0530

    font change

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -15,7 +15,7 @@ const Header = ({setdarkmodeon,darkmodeon}) => {
         {/* Ashoka Logo on the left */}
         <h2 className="text-4xl font-medium flex items-center">
           <img
-            className="object-cover max-sm:h-10 h-24"
+            className="object-cover max-sm:h-10 h-20"
             src="https://logos.textgiraffe.com/logos/logo-name/Ashoka-designstyle-birthday-m.png"
             alt="Ashoka Logo"
           />
@@ -24,11 +24,11 @@ const Header = ({setdarkmodeon,darkmodeon}) => {
          
 
         <div className="flex gap-8 items-center ">
-        <p onClick={()=>setdarkmodeon(prev=>!prev)} className="text-5xl flex cursor-pointer">
+        <p onClick={()=>setdarkmodeon(prev=>!prev)} className="text-3xl flex cursor-pointer">
             <i className={`max-sm:text-2xl ${darkmodeon?'ri-sun-line':'ri-moon-clear-fill'} `}></i>
           </p>
          <div   className="flex gap-3 cursor-pointer items-center  rounded-full border-2 max-sm:px-3 px-6  mx-sm:py-1 md:py-3  max-sm:mr-1 mr-12 border-black h-fit ">
-         <button className="max-sm:text-xl text-2xl">Contact</button>
+         <button className="max-sm:text-xl text-xl">Contact</button>
           <p className="text-2xl ">
             <i className="ri-arrow-right-up-line"></i>
           </p>
@@ -39,12 +39,12 @@ const Header = ({setdarkmodeon,darkmodeon}) => {
 
       <div className="flex gap-3 w-full justify-center  fixed bottom-2">
         <div className="flex w-fit hover:bg-slate-800 bg-slate-400 gap-6 px-6 py-1 rounded-full">
-       <a className="text-5xl rounded-full bg-white px-1 cursor-pointer" href="#home"> <i className="ri-home-2-line"></i></a>
+       <a className=" rounded-full text-2xl px-2 bg-white cursor-pointer" href="#home"> <i  className="ri-home-2-line"></i></a>
           <img
             onClick={() =>
               window.open("https://www.instagram.com/ashoklande73/?hl=en")
             }
-            className="h-14 cursor-pointer object-center rounded-full"
+            className="h-10 cursor-pointer object-center rounded-full"
             src="https://img.freepik.com/premium-vector/instagram-logo-vector_768467-330.jpg"
             alt="Instagram Logo"
           />
@@ -52,7 +52,7 @@ const Header = ({setdarkmodeon,darkmodeon}) => {
             onClick={() =>
               window.open("https://www.linkedin.com/in/ashok-lande-492431230/")
             }
-            className="object-cover cursor-pointer object-center h-14 rounded-full"
+            className="object-cover cursor-pointer object-center h-10 rounded-full"
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfRQj_W8078FCL2-vDL5oWTJIVLhz8OH5g-A&s"
             alt="LinkedIn Logo"
           />
@@ -60,7 +60,7 @@ const Header = ({setdarkmodeon,darkmodeon}) => {
             onClick={() =>
               window.open("https://github.com/Ashoklande", "_blank")
             }
-            className="object-cover cursor-pointer object-center h-14 rounded-full"
+            className="object-cover cursor-pointer object-center h-10 rounded-full"
             src="https://w7.pngwing.com/pngs/914/758/png-transparent-github-social-media-computer-icons-logo-android-github-logo-computer-wallpaper-banner-thumbnail.png"
             alt="GitHub Logo"
           />
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -22,19 +22,19 @@ const Home = ({darkmodeon}) => {
         transition={{duration:0.8,type:'spring',stiffness:100}}
         className="photo md:w-1/2  items-center gap-2 flex flex-col justify-center">
          <img
-            className="rounded-full object-cover bg-green-700 border-[30px] border-green-500 w-full h-auto max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl"
+            className="rounded-full object-cover bg-green-700 border-[30px] border-green-500 w-full h-auto max-w-xs sm:max-w-sm md:max-w-sm lg:max-w-md xl:max-w-md"
             src={myimage}
             alt="Description"
           />
-          <h2 className=" md:text-4xl  font-mono">
+          <h2 className=" md:text-2xl  font-mono">
           Hi I'am a Ashok Lande 👋
           </h2>
         </motion.div>
         <div className="content  md:w-1/2  flex flex-col items-start md:justify-center">
-          <h1 className={` ${darkmodeon?'text-white':''}  md:text-5xl max-sm:text-xl  font-mono text-3xl text-black font-semibold`}>
+          <h1 className={` ${darkmodeon?'text-white':''}  md:text-3xl max-sm:text-xl  font-mono text-3xl text-black font-semibold`}>
             <br />
             {'<>'}  I&apos;m a
-            <span className="md:text-5xl ml-2 text-green-700 max-sm:text-xl text-3xl font-medium">
+            <span className="md:text-3xl ml-2 text-green-700 max-sm:text-xl text-3xl font-medium">
               {typeEffect}
             </span>
             <Cursor />
@@ -45,19 +45,19 @@ const Home = ({darkmodeon}) => {
                initial={{x:-20,opacity:0}}
                whileInView={{x:1,opacity:1}}
                transition={{duration:0.9,delay:0.6}}
-             className="md:text-xl font-mono text-lg ">
+             className="md:text-base font-mono text-sm ">
               Hi, I'm , a passionate web developer. I specialize in creating
               intuitive and responsive websites using modern technologies.
-               {/* Let's
+               Let's
               collaborate and build something amazing together! Feel free to
               replace Ashoka with your actual name and adjust the phrasing to
-              match your style or area of expertise. */}
+              match your style or area of expertise.
             </motion.p>
           </div>
           <div
            className="gap-3  flex mt-10">
-            <button className={` ${darkmodeon?'border-white':''} text-2xl px-6 max-sm:text-base border-2 py-3 bg-black hover:bg-slate-300 hover:text-black text-white rounded-3xl`}>Contact me <i class="ri-arrow-right-fill"></i></button>
-            <button onClick={()=>window.open(resume)}  className={` ${darkmodeon?'border-white  hover:text-black':''} max-sm:text-base text-2xl px-6 py-3 hover:bg-slate-300 border-2  border-slate-600 rounded-3xl`}>my resume <i className="ri-download-2-line"></i></button>
+            <button className={` ${darkmodeon?'border-white':''} text-xl px-6 max-sm:text-base border-2 py-3 bg-black hover:bg-slate-300 hover:text-black text-white rounded-3xl`}>Contact me <i class="ri-arrow-right-fill"></i></button>
+            <button onClick={()=>window.open(resume)}  className={` ${darkmodeon?'border-white  hover:text-black':''} max-sm:text-base text-xl px-6 py-3 hover:bg-slate-300 border-2  border-slate-600 rounded-3xl`}>my resume <i className="ri-download-2-line"></i></button>
           </div>
         </div>
       </div>
